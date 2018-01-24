b28384002b2c686b99045ba1865d9420083e3ed5
Jan Busse <jan.busse22@yahoo.de>
2018-01-24 21:13:06 +0100

    Added methods to read data from csv files and process it

--- filter_data.py
+++ filter_data.py
@@ -7,7 +7,8 @@ Created on Sun Jan 21 17:47:36 2018
 Select data
 """
 import os, fnmatch
-import shutil
+import pandas
+import numpy as np
 
 def find(pattern, path):
     result = []
@@ -28,7 +29,29 @@ def ticker2fn(ticker):
     ticker = ticker + ".*"
     return ticker
     
-
-
-
+# reads all csv files in the given directory and returns a list of the data 
+# frames
+def read_data(path, sep: str):
+    data_frames = []
+    for file in os.listdir(path):
+        if file[-4:] == '.csv':
+            data_frames.append(pandas.read_csv(path + '/' + file ,sep=sep))
+    
+    return data_frames
+    
+        
+# converts a list of data frames to a numpy array
+def convert_dataset(data_frames: list, columns: list) -> np.array:
+    num_rows = len(data_frames)
+    num_cols = data_frames[0].shape[0]
+    
+    if len(columns) > 1:
+        n_array = np.zeros((num_rows, num_cols, len(columns)))
+    else:
+        n_array = np.zeros((num_rows, num_cols))
+    
+    for i, data_frame in enumerate(data_frames):
+        n_array[i] = data_frame.as_matrix(columns).reshape(num_cols)
+    
+    return n_array
 

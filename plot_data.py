3ce74d3c426944a66136e78724b35f52a36f7456
Jan Busse <jan.busse22@yahoo.de>
2018-02-15 23:49:12 +0100

    Added results k-means for adjusted stock prizes. Added figure of adj prizes, crosstab and explanation in latex.

--- plot_data.py
+++ plot_data.py
@@ -15,15 +15,17 @@ pgf_with_rc_fonts = {
     "font.serif": [],                   # use latex default serif font
     "font.sans-serif": ["DejaVu Sans"], # use a specific sans-serif font
 }
+plt.rcParams['axes.unicode_minus'] = False
+mpl.rcParams['axes.unicode_minus'] = False
 mpl.rcParams.update(pgf_with_rc_fonts)
 
 
 plt.figure(figsize=(4.5,2.5))
-plt.plot(days, stocks['aapl'], '#4CAF50', label='Apple')
-plt.plot(days, stocks['msft'], '#673AB7', label='Microsoft')
-plt.fill_between(days, stocks['aapl'], stocks['msft'], facecolor='#E1F5FE')
+plt.plot(days, stocks_adj['aapl'], '#4CAF50', label='Apple')
+plt.plot(days, stocks_adj['msft'], '#673AB7', label='Microsoft')
+plt.fill_between(days, stocks_adj['aapl'], stocks_adj['msft'], facecolor='#E1F5FE')
 plt.tight_layout()
-plt.xlabel("Days")
-plt.ylabel(r"Stock Prize [\$]")
-plt.legend(bbox_to_anchor=(0.95, 0.65))
-plt.savefig('appl-vs-msft.pgf')
+plt.xlabel("days")
+plt.ylabel(r"stock prize in dollar")
+plt.legend(bbox_to_anchor=(0.98, 0.35))
+plt.savefig('appl-vs-msft_adj.pgf')
